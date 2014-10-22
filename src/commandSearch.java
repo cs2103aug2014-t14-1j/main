cbb89732b8abc459fc1ac0cda2c725ff8a84db7a
caidicaidi123 <caidicaidi123@gmail.com>
2014-10-23 00:39:15 +0800

    search

--- src/commandSearch.java
+++ src/commandSearch.java
@@ -5,31 +5,38 @@ package todo_manager;
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class CommandSearch {
+public class commandSearch {
+	Storage storage;
+	private LinkedList<Entry> entryList;
 	
-	private ArrayList<Entry> storage;
 	
-	public CommandSearch(ArrayList<Entry> storage){
-		this.storage = storage;
+	public commandSearch(LinkedList<Entry> list){
+		this.entryList = list;
 	}
 	
-	public ArrayList<Entry> search(String searchContent){
+	public LinkedList<Entry> search(String searchContent){
+		searchContent = searchContent.trim();
 		ValidationCheck validCheck = new ValidationCheck();
 		boolean isDate = validCheck.isValidDate(searchContent);
 		
-		ArrayList<Entry> searchResult = new ArrayList<Entry>();
+		System.out.println("search content is date: "+isDate);
+		
+		LinkedList<Entry> searchResult = new LinkedList<Entry>();
 		
 		if(isDate){
-			for(Entry entry: storage){
+			for(Entry entry: entryList){
 				if(entry.getStartingDate() == searchContent ||entry.getEndingDate() == searchContent ){
+					System.out.println(entry.getStartingDate()+"ending date is "+entry.getEndingDate());
 					searchResult.add(entry);
+					System.out.println("There is a date match");
 				}
 			}
 		}
 		else{
 			searchContent = searchContent.toLowerCase();
-			for(Entry entry: storage){
+			for(Entry entry: entryList){
 				String temp = entry.getName().toLowerCase();
 				if(temp.contains(searchContent)){
 					searchResult.add(entry);
